d302364b725dcf93bc0427c39132baef3100212c
Kevin Perez <kevinp@cronapis.com>
2021-09-19 15:04:18 -0400

    proyecto dynamodb

---
+++ spring-boot-dynamodb/src/test/java/com/kurodevs/app/springbootdynamodb/SpringBootDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurodevs.app.springbootdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
