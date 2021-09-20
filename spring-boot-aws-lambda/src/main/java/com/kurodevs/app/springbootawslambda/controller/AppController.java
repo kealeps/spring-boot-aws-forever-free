3f738b166c5ca1e920ecfd09952d3fe9b2cdd4f5
Kevin Perez <mr.kv17.kp@gmail.com>
2021-09-19 20:46:41 -0400

    AWS Lambda

---
+++ spring-boot-aws-lambda/src/main/java/com/kurodevs/app/springbootawslambda/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.kurodevs.app.springbootawslambda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+    
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "hello-world!";
+    }
+}
